08afe91817addfbcaef021830fb7fb33cbdddada
Sibel97 <s_hassan97@hotmail.co.uk>
2022-08-04 13:03:06 +0000

    script change

--- jenkins-script.sh
+++ jenkins-script.sh
@@ -11,6 +11,18 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 python3 -m pytest --cov=application --cov-report=html
+ssh jenkins@prod-server << EOF
+if [ -d projet]: then 
+    cd project && git pull origin master
+else 
+    git clone https://github.com/Sibel97/ProjectBookStore project
+    cd project
+fi
+sudo apt install python3 python3-pip python3-venv -y
 
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
 python3 create.py
-python3 app.py
+python3 -m gunicorn -D --bind 0.0.0.0:5000 --workers 4 app:app
+EOF
